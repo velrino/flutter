5c5944ebea7d1ffbd04e94fe8e74e84eaa6bac7f
velrino <denismagalhaesvelrino@gmail.com>
2020-02-07 23:11:42 -0300

    :art: componentizing all

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,20 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
+class AutoInputInferface {
+  final String label;
+
+  AutoInputInferface(this.label);
+}
+
 /// This Widget is the main application widget.
 class MyApp extends StatelessWidget {
   static const String _title = 'AutoFront';
 
   @override
   Widget build(BuildContext context) {
+    const teste = 'lorem';
+
     return MaterialApp(
       title: _title,
       home: Scaffold(
@@ -15,32 +23,62 @@ class MyApp extends StatelessWidget {
         body: ListView(
             shrinkWrap: true,
             padding: const EdgeInsets.all(20.0),
-            children: [InputWidget()]),
+            children: [
+              TextWidget(),
+              InputWidgetState(autoInput: new AutoInputInferface("Lorem")),
+              InputWidgetState(autoInput: new AutoInputInferface("Ipsum"))
+            ]),
       ),
     );
   }
 }
 
-class InputWidget extends StatefulWidget {
-  InputWidget({Key key}) : super(key: key);
-
+class TextWidget extends StatelessWidget {
   @override
-  InputWidgetState createState() => InputWidgetState();
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: const EdgeInsets.symmetric(vertical: 16.0),
+        child: Text('Hello World'));
+  }
 }
 
-class InputWidgetState extends State<InputWidget> {
+class ButtonWidget extends StatelessWidget {
   final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
-    return Form(
-      key: _formKey,
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 16.0),
+      child: RaisedButton(
+        onPressed: () {
+          // Validate will return true if the form is valid, or false if
+          // the form is invalid.
+          if (_formKey.currentState.validate()) {
+            // Process data.
+          }
+        },
+        child: Text('Submit'),
+      ),
+    );
+  }
+}
+
+class InputWidgetState extends StatelessWidget {
+  final AutoInputInferface autoInput;
+
+  // In the constructor, require a Person
+  InputWidgetState({Key key, @required this.autoInput}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 16.0),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
           TextFormField(
             decoration: InputDecoration(
-              labelText: "Enter your e-mail",
+              labelText: autoInput.label,
               fillColor: Colors.white,
               border: OutlineInputBorder(
                 borderRadius: BorderRadius.circular(25.0),
@@ -53,19 +91,6 @@ class InputWidgetState extends State<InputWidget> {
               }
               return null;
             },
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 16.0),
-            child: RaisedButton(
-              onPressed: () {
-                // Validate will return true if the form is valid, or false if
-                // the form is invalid.
-                if (_formKey.currentState.validate()) {
-                  // Process data.
-                }
-              },
-              child: Text('Submit'),
-            ),
           ),
         ],
       ),
